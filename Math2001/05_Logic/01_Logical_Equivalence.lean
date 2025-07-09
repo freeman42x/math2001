305217258ad33a5555b48c6d3837c04c869f222b
Razvan Flavius Panda <razvan.panda@gmail.com>
2025-07-09 23:04:09 +0300

    #

--- Math2001/05_Logic/01_Logical_Equivalence.lean
+++ Math2001/05_Logic/01_Logical_Equivalence.lean
@@ -91,10 +91,16 @@ example (P : α → Prop) : ¬ (∃ x, P x) ↔ ∀ x, ¬ P x := by
 
 
 example {P Q : Prop} (h : P ∧ Q) : P ∨ Q := by
-  sorry
+  obtain ⟨p, q⟩ := h
+  left
+  apply p
 
 example {P Q R : Prop} (h1 : P → Q) (h2 : P → R) (h3 : P) : Q ∧ R := by
-  sorry
+  constructor
+  · apply h1 at h3
+    apply h3
+  · apply h2 at h3
+    apply h3
 
 example (P : Prop) : ¬(P ∧ ¬ P) := by
   sorry
