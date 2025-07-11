ab95ea6f69326c96109cdbf92e446c247e0e06db
Razvan Flavius Panda <razvan.panda@gmail.com>
2025-07-11 22:31:29 +0300

    #

--- Math2001/05_Logic/03_Negation_Algorithm.lean
+++ Math2001/05_Logic/03_Negation_Algorithm.lean
@@ -178,5 +178,7 @@ example : ¬ ∃ a : ℤ, ∀ n : ℤ, 2 * a ^ 3 ≥ n * a + 7 := by
 example {p : ℕ} (hp : ¬ Prime p) (hp2 : 2 ≤ p) : ∃ m, 2 ≤ m ∧ m < p ∧ m ∣ p := by
   have H : ¬ (∀ (m : ℕ), 2 ≤ m → m < p → ¬m ∣ p)
   · intro H
-    sorry
-  sorry
+    have := prime_test hp2 H
+    contradiction
+  push_neg at H
+  exact H
